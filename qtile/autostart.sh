4464015d6638b554c9123addadc1cdbc7bbae696
Fool543 <120022950+Fool543@users.noreply.github.com>
2023-03-08 07:08:35 +0000

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 nm-applet &
-picom -b &
+picom --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350 --backend glx --vsync --corner-radius 12 -b &
 /usr/lib/polkit-kde-authentication-agent-1 &
